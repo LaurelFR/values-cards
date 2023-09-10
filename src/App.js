5be4e7b0383b68065f535da690da8a2cf303096c
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-09-09 20:05:59 -0600

    Added data to array1 and second map

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import "./App.css";
 const App = () => {
   // Initialize four arrays and their state variables
   const [array1, setArray1] = useState([
-    `Acceptance: to be accepted as I am`,
-    "String 2",
-    "String 3",
+    ["Acceptance", "to be accepted as I am"],
+    ["Accuracy", "to be accurate in my opinions and beliefs"],
+    ["Achievement", "to have important accomplishments"],
+    ["Adventure", "to have new and exciting experiences"],
+    ["Attractiveness", "to be physically attractive"],
+    ["Authority", "to be in charge of and responsible for others"],
+    ["Autonomy", "to be self-determined and independent"],
+    ["Beauty", "to appreciate beauty around me"],
+    ["Caring", "to take care of others"],
+    ["Challenge", "to take on difficult tasks and problems"],
   ]);
   const [array2, setArray2] = useState([]);
   const [array3, setArray3] = useState([]);
@@ -52,27 +59,29 @@ const App = () => {
             <h2>Array 1: Cards</h2>
             <hr />
             <ul>
-              {array1.map((string, index) => (
-                <li key={index} className="card">
-                  {string}
-                  <div>
+              {array1.map((strings, index) => (
+                <li key={index} className="card mb-2">
+                  {strings.map((string, index) => (
+                    <span key={index}>{string}</span>
+                  ))}
+                  <div className="mt-3">
                     <button
                       className="btn btn-2"
-                      onClick={() => moveString(string, array2, setArray2)}
+                      onClick={() => moveString(strings, array2, setArray2)}
                     >
-                      Array 2
+                      2
                     </button>
                     <button
                       className="btn btn-3"
-                      onClick={() => moveString(string, array3, setArray3)}
+                      onClick={() => moveString(strings, array3, setArray3)}
                     >
-                      Array 3
+                      3
                     </button>
                     <button
                       className="btn btn-4"
-                      onClick={() => moveString(string, array4, setArray4)}
+                      onClick={() => moveString(strings, array4, setArray4)}
                     >
-                      Array 4
+                      4
                     </button>
                   </div>
                 </li>
@@ -83,21 +92,25 @@ const App = () => {
             <h2>2: Very Important</h2>
             <hr />
             <ul>
-              {array2.map((string, index) => (
+              {array2.map((strings, index) => (
                 <li key={index} className="card">
-                  {string}{" "}
-                  <button
-                    className="btn btn-3"
-                    onClick={() => moveString2(string, array3, setArray3)}
-                  >
-                    Move to Array 3
-                  </button>
-                  <button
-                    className="btn btn-4"
-                    onClick={() => moveString2(string, array4, setArray4)}
-                  >
-                    Move to Array 4
-                  </button>
+                  {strings.map((string, index) => (
+                    <span key={index}>{string}</span>
+                  ))}
+                  <div>
+                    <button
+                      className="btn btn-3"
+                      onClick={() => moveString2(strings, array3, setArray3)}
+                    >
+                      3
+                    </button>
+                    <button
+                      className="btn btn-4"
+                      onClick={() => moveString2(strings, array4, setArray4)}
+                    >
+                      4
+                    </button>
+                  </div>
                 </li>
               ))}
             </ul>
@@ -106,21 +119,25 @@ const App = () => {
             <h2>3: Important</h2>
             <hr />
             <ul>
-              {array3.map((string, index) => (
+              {array3.map((strings, index) => (
                 <li key={index} className="card">
-                  {string}{" "}
-                  <button
-                    className="btn btn-2"
-                    onClick={() => moveString3(string, array2, setArray2)}
-                  >
-                    Move to Array 2
-                  </button>
-                  <button
-                    className="btn btn-4"
-                    onClick={() => moveString3(string, array4, setArray4)}
-                  >
-                    Move to Array 4
-                  </button>
+                  {strings.map((string, index) => (
+                    <span key={index}>{string}</span>
+                  ))}
+                  <div>
+                    <button
+                      className="btn btn-2"
+                      onClick={() => moveString3(strings, array2, setArray2)}
+                    >
+                      2
+                    </button>
+                    <button
+                      className="btn btn-4"
+                      onClick={() => moveString3(strings, array4, setArray4)}
+                    >
+                      4
+                    </button>
+                  </div>
                 </li>
               ))}
             </ul>
@@ -129,21 +146,25 @@ const App = () => {
             <h2>4: Not Important</h2>
             <hr />
             <ul>
-              {array4.map((string, index) => (
+              {array4.map((strings, index) => (
                 <li key={index} className="card">
-                  {string}
-                  <button
-                    className="btn btn-2"
-                    onClick={() => moveString4(string, array2, setArray2)}
-                  >
-                    Move to Array 2
-                  </button>
-                  <button
-                    className="btn btn-3"
-                    onClick={() => moveString4(string, array3, setArray3)}
-                  >
-                    Move to Array 3
-                  </button>
+                  {strings.map((string, index) => (
+                    <span key={index}>{string}</span>
+                  ))}
+                  <div>
+                    <button
+                      className="btn btn-2"
+                      onClick={() => moveString4(strings, array2, setArray2)}
+                    >
+                      2
+                    </button>
+                    <button
+                      className="btn btn-3"
+                      onClick={() => moveString4(strings, array3, setArray3)}
+                    >
+                      3
+                    </button>
+                  </div>
                 </li>
               ))}
             </ul>
