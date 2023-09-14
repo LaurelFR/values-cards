020004ca7dff58b8b0a9811b88c2ba896a62cb73
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-09-14 16:18:30 -0600

    Added styling

--- src/CardSort.js
+++ src/CardSort.js
@@ -41,10 +41,24 @@ export default function CardSort(data) {
 
   return (
     <div className="container">
-      <h1 className="mt-3 mb-5">Values Card Sort</h1>
+      <h1 className="mt-3 mb-2">Personal Values Card Sort</h1>
+      <p className="text-center mb-1">
+        Based on Personal Values Card Sort by W.R. Miller, J.C'de Baca, D.B.
+        Matthews, P.L. Wilbourne, University of New Mexico, 2001
+      </p>
+      <p className="text-center mb-2">
+        This project was coded by <strong>Laurel Rubel</strong> and is{" "}
+        <a
+          href="https://github.com/LaurelFR/values-cards"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </p>
       <div className="row">
-        <div className="col">
-          <h2>Array 1: Cards</h2>
+        <div className="col card-column">
+          <h2>Values Cards</h2>
           <hr />
           <ul>
             {array1.map((strings, index) => (
@@ -52,111 +66,115 @@ export default function CardSort(data) {
                 {strings.map((string, index) => (
                   <span key={index}>{string}</span>
                 ))}
-                <div className="mt-3">
+                <div className="mt-2">
                   <button
                     className="btn btn-2"
                     onClick={() => moveString(strings, array2, setArray2)}
                   >
-                    2
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
                   </button>
                   <button
                     className="btn btn-3"
                     onClick={() => moveString(strings, array3, setArray3)}
                   >
-                    3
+                    <i className="fa-solid fa-star"></i>
                   </button>
                   <button
                     className="btn btn-4"
                     onClick={() => moveString(strings, array4, setArray4)}
                   >
-                    4
+                    <i className="fa-solid fa-circle-xmark"></i>
                   </button>
                 </div>
               </li>
             ))}
           </ul>
         </div>
-        <div className="col">
+        <div className="col very-important">
           <h2>
-            2: Very Important <i class="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i>
+            <i className="fa-solid fa-star"></i> Very Important
           </h2>
           <hr />
           <ul>
             {array2.map((strings, index) => (
-              <li key={index} className="card mb-2">
+              <li key={index} className="card shadow mb-2">
                 {strings.map((string, index) => (
                   <span key={index}>{string}</span>
                 ))}
-                <div className="mt-3">
+                <div className="mt-2">
                   <button
                     className="btn btn-3"
                     onClick={() => moveString2(strings, array3, setArray3)}
                   >
-                    3
+                    <i className="fa-solid fa-star"></i>
                   </button>
                   <button
                     className="btn btn-4"
                     onClick={() => moveString2(strings, array4, setArray4)}
                   >
-                    4
+                    <i className="fa-solid fa-circle-xmark"></i>
                   </button>
                 </div>
               </li>
             ))}
           </ul>
         </div>
-        <div className="col">
+        <div className="col important">
           <h2>
-            3: Important <i class="fa-solid fa-square"></i>
+            <i className="fa-solid fa-star"></i> Important
           </h2>
           <hr />
           <ul>
             {array3.map((strings, index) => (
-              <li key={index} className="card mb-2">
+              <li key={index} className="card shadow mb-2">
                 {strings.map((string, index) => (
                   <span key={index}>{string}</span>
                 ))}
-                <div className="mt-3">
+                <div className="mt-2">
                   <button
                     className="btn btn-2"
                     onClick={() => moveString3(strings, array2, setArray2)}
                   >
-                    2
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
                   </button>
                   <button
                     className="btn btn-4"
                     onClick={() => moveString3(strings, array4, setArray4)}
                   >
-                    4
+                    <i className="fa-solid fa-circle-xmark"></i>
                   </button>
                 </div>
               </li>
             ))}
           </ul>
         </div>
-        <div className="col">
+        <div className="col not-important">
           <h2>
-            4: Not Important <i class="fa-solid fa-circle-xmark"></i>
+            <i className="fa-solid fa-circle-xmark"></i> Not Important
           </h2>
           <hr />
           <ul>
             {array4.map((strings, index) => (
-              <li key={index} className="card mb-2">
+              <li key={index} className="card shadow mb-2">
                 {strings.map((string, index) => (
                   <span key={index}>{string}</span>
                 ))}
-                <div className="mt-3">
+                <div className="mt-2">
                   <button
                     className="btn btn-2"
                     onClick={() => moveString4(strings, array2, setArray2)}
                   >
-                    2
+                    <i className="fa-solid fa-star"></i>
+                    <i className="fa-solid fa-star"></i>
                   </button>
                   <button
                     className="btn btn-3"
                     onClick={() => moveString4(strings, array3, setArray3)}
                   >
-                    3
+                    <i className="fa-solid fa-star"></i>
                   </button>
                 </div>
               </li>
